cfdc705e84666296486c3d3c4498a678a83b6ed2
rafal-szatkiewicz <rafalszatkiewicz2@gmail.com>
2025-08-03 19:53:23 +0200

    remove .vercel

--- public/index.js
+++ public/index.js
@@ -131,6 +131,13 @@ buses.forEach(bus => {
 }
 
 async function init() {
+
+  fetch('/dark_matter.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  });
+
   const map = new Map({
     //style: 'https://tiles.openfreemap.org/styles/liberty',
     style: "./.vercel/output/style.json",
