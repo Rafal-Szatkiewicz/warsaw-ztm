e5460792144ec53ad7265c2acae6d26dc5fff6f8
rafal-szatkiewicz <rafalszatkiewicz2@gmail.com>
2025-08-03 17:39:31 +0200

    opacity

--- public/index.js
+++ public/index.js
@@ -104,8 +104,8 @@ buses.forEach(bus => {
       ? Array.from({ length: INTERP_POINTS }, (_, j) => (j / (INTERP_POINTS - 1)) * MIN_SEGMENT_DURATION)
       : Array(INTERP_POINTS).fill(0); // statyczny, nie animowany
 
-    const opacityFactor = i / hist.length;
-    const alpha = Math.round(255 * Math.pow(1 - opacityFactor, 1.5));
+    const ageFactor = (hist.length - i) / hist.length;
+    const alpha = Math.round(255 * Math.pow(ageFactor, 1.5));
     trips.push({
       path,
       timestamps,
